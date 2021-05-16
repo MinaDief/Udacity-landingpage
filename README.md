7888b5f55ccf2adf3e8a0512cb1f76f1dccb704d
Mina Samir Kamel Hanna <mina.dief@gmail.com>
2021-05-16 22:33:35 +0200

    Added Javascript scrolling feature

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 * [Instructions](#instructions)
 * [Usability](#usability)
+* [References](#references)
+
 
 ## Instructions
 I have added the interactivity for this landing page using Javascript code, also I have added some CSS and HTML code, to match the javascript code.
@@ -13,8 +15,7 @@ Click on any section on the Nav bar at the top of the web page, and it will scro
 Scroll through sections and the background of the current section will be viewed in a special background and animation.
 
 
-Kindly note that I have made most of the logic on this website, and for few parts,  I have checked some websites to get the right syntax for the code.
-those websites are like:
+## References
 https://stackoverflow.com/
 https://www.w3schools.com/
 https://developer.mozilla.org/en-US/
--- js/app.js
+++ js/app.js
@@ -34,37 +34,34 @@ const sectionsList = document.querySelectorAll("section");
 const navBarMenu= document.querySelector('#navbar__list');
 
 
+
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
 function createNavBarSections(){
+    let i=0;
     for(section of sectionsList)
     {
         let sectionName = section.getAttribute('data-nav');
         let sectionLink = section.getAttribute('id');
         const newSection = document.createElement('li');
-        newSection.innerHTML=`<a href='#${sectionLink}'>${sectionName}</a>`;
-        //test: ok, this makes an id for each created section to be used to activate the selected section
-       const tempClassName= "id__of__"+sectionLink;
-        newSection.setAttribute("id",tempClassName);
+        newSection.innerHTML=`<a>${sectionName}</a>`;
         
-        
+       //test: ok, this makes an id for each created section to be used to activate the selected section
+       const tempIDName= "id__of__"+sectionLink;
+        newSection.setAttribute("id",tempIDName);
         navBarMenu.appendChild(newSection);
     }
 }
 
 // Add class 'active' to section when near top of viewport
-
 function SectionInViewport(sect) {
     const boundingClient = sect.getBoundingClientRect();
     const sectionStyle = getComputedStyle(sect);  //this puts the css style of the current section into the variable style
     const sectionHeightString = sectionStyle.height.match(/\d+/g); //this line gets the height of the section in pixels "242px" and converts it into number without px "242"
     const sectionHeightNumber = parseInt(sectionHeightString, 10);
-
-
-   
     return  boundingClient.top >= 0 && boundingClient.top <sectionHeightNumber ; // check if the section is in the viewport
 }
 
@@ -75,21 +72,13 @@ function activateCurrentSection (){
         const sectionId ="#id__of__"+ section.getAttribute('id');
         if(SectionInViewport(section)){
             section.classList.add("your-active-class");
-            
-            
             document.querySelector(sectionId).style.backgroundColor="aqua";
-            
         }
         else { 
          section.classList.remove("your-active-class");
          document.querySelector(sectionId).style.backgroundColor="white";
         }
-        //for every item in the navbarmenu : check if it's related to the item in the view port and if it's , make a heighlight
-        navBarList = document.querySelectorAll('#navbar__list li')
-        for(navBarItem of navBarList){
-           // console.log(navBarItem.innerHTML);
-          //  if(navBarItem.innerHTML.)
-        }
+        
     }
     
 
@@ -107,8 +96,21 @@ createNavBarSections();
 
 
 // Scroll to anchor ID using scrollTO event
+ 
+id__of__section1.addEventListener('click', function (){
+    document.getElementById("section1").scrollIntoView();
+});
+id__of__section2.addEventListener('click', function (){
+    document.getElementById("section2").scrollIntoView();
+});
+id__of__section3.addEventListener('click', function (){
+    document.getElementById("section3").scrollIntoView();
+});
+id__of__section4.addEventListener('click', function (){
+    document.getElementById("section4").scrollIntoView();
+});
 
-
+//document.getElementById(section).scrollIntoView();
 /**
  * End Main Functions
  * Begin Events
@@ -118,7 +120,7 @@ createNavBarSections();
 
 
 
-// Scroll to section on link click
+
 
 // Set sections as active
 document.addEventListener('scroll', activateCurrentSection);
